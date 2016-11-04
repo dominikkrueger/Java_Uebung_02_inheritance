a1f1b9216edecead1ff2f7717166c684ea5a4f75
Dominik Krueger <dominik.krueger@edu.fh-joanneum.at>
2016-11-04 15:32:19 +0100

    Initial commit

---
+++ src/fh/java/geometry/model/curved/CurvedShape.java
@@ -0,0 +1,9 @@
+package fh.java.geometry.model.curved;
+
+import fh.java.geometry.model.GeometricShape;
+
+public abstract class CurvedShape extends GeometricShape {
+
+//because we wont to implement the methods of GeometricShape, this has to be also an abstract class
+
+}
